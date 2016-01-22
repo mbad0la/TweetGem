9141e4f8e4ec89a022b687a0dac79cdcc8be7f77
Mayank Badola <badola21295@gmail.com>
2016-01-22 19:13:36 +0530

    https://twittercommunity.com/t/post-oauth2-token-application-only-authentication-always-404/10822/22

--- lib/tweetgem.rb
+++ lib/tweetgem.rb
@@ -1,10 +1,19 @@
-require 'httparty'
-require_relative 'auth/appauth'
-
+require 'http'
+require 'base64'
 class Tweetapi
-
-  def initialize()
-    appauthInit()
+  KEY = 'CV2aMxKrMiGZjGLdw9YvdLTpF'
+  SECRET = 'aPKOronl16SNDAzo681gtSkQRFCfWPl6bROqpCCTTsPzithFuO'
+  def initialize
+    @encoded = Base64.encode64(KEY<<':'<<SECRET)
+    @basic_auth = ""
+    @encoded.split("").each {|c|
+      unless c == "\n"
+        @basic_auth<<c
+      end
+    }
+    @basic_auth = "Basic "<<@basic_auth
+    response = HTTP.auth(@basic_auth).headers("Content-Type" => "application/x-www-form-urlencoded;charset=UTF-8").post("https://api.twitter.com/oauth2/token", :body => "grant_type=client_credentials")
+    puts response.body
   end
   
 end
